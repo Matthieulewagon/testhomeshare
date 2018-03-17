d403b456dfe5f8365ef991071a04a7691678a7d1
Matthieu Lombard <matthieu.lombard@hotmail.be>
2018-03-17 14:22:59 +0100

    skip validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,21 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   after_create :send_welcome_email
 
+  attr_accessor :skip_password_validation
+
+  protected
+
+  def password_required?
+    return false if skip_password_validation
+    super
+  end
+
   private
 
   def send_welcome_email
     UserMailer.welcome(self).deliver_now
   end
 end
+
+user.skip_password_validation = true
+user.save
