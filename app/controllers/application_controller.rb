98714564cea8ed60d648d8fdf94ad04b506d32e1
Matthieu Lombard <matthieu.lombard@hotmail.be>
2018-03-18 18:27:10 +0100

    migr

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,9 +19,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :city, :zip, :telephone, :mobile, :country, :email, :organisation, :address, :state])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :city, :zip, :telephone, :mobile, :country, :email, :organisation, :address, :state, :check, :members, :prices, :friend_name, :friend_address])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :city, :zip, :telephone, :mobile, :country, :email, :organisation, :address, :state])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :city, :zip, :telephone, :mobile, :country, :email, :organisation, :address, :state, :check, :members, :prices, :friend_name, :friend_address])
   end
 end
