caa7af6026b99ff6bbc272a13982b0d8ba9f84da
Matthieu Lombard <matthieu.lombard@hotmail.be>
2018-03-23 14:17:57 +0100

    test

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,13 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
+
+  # def message
+  #   mail(
+  #     :subject => 'Hello from Postmark',
+  #     :to  => 'matthieulombard@1toit2ages.be',
+  #     :from => 'matthieulombard@1toit2ages.be',
+  #     :html_body => '<strong>Hello</strong> dear Postmark user.',
+  #     :track_opens => 'true')
+  # end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -11,14 +11,5 @@ class UserMailer < ApplicationMailer
     mail(to: @user.email, subject: 'Confirmation: You are registered')
     # This will render a view in `app/views/user_mailer`!
   end
-
-  def message
-    mail(
-      :subject => 'Hello from Postmark',
-      :to  => 'matthieulombard@1toit2ages.be',
-      :from => 'matthieulombard@1toit2ages.be',
-      :html_body => '<strong>Hello</strong> dear Postmark user.',
-      :track_opens => 'true')
-  end
 end
 
