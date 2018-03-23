28eb4869c538453f9f4ef46e234fbb01856a1a35
Matthieu Lombard <matthieu.lombard@hotmail.be>
2018-03-23 14:29:28 +0100

    test

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,13 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
-
-  # def message
-  #   mail(
-  #     :subject => 'Hello from Postmark',
-  #     :to  => 'matthieulombard@1toit2ages.be',
-  #     :from => 'matthieulombard@1toit2ages.be',
-  #     :html_body => '<strong>Hello</strong> dear Postmark user.',
-  #     :track_opens => 'true')
-  # end
 end
