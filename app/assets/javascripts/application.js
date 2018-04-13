2b181286950c16078e0f1653caa8852930dfadd7
Matthieu Lombard <matthieu.lombard@hotmail.be>
2018-04-13 21:23:12 +0200

    javascr

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -38,4 +38,26 @@ window.onclick = function(event) {
   }
 }
 
+// $("a[href*='" + location.pathname + "']").addClass("current");
+
+// $(function() {
+//   $('nav a[href^="/' + location.pathname.split("/")[1] + '"]').addClass('current');
+// });
+
+if (location.pathname !== '/') {
 $("a[href*='" + location.pathname + "']").addClass("current");
+} else {
+var home = document.getElementById("home").getElementsByTagName('a')[0];
+home.className = 'current';
+}
+
+// $(function(){
+//     var current = location.pathname;
+//     $('#nav li a').each(function(){
+//         var $this = $(this);
+//         // if the current path is like this link, make it active
+//         if($this.attr('href').indexOf(current) !== -1){
+//             $this.addClass('active');
+//         }
+//     })
+// })
